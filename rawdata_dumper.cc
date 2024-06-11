2151f01fb4ef5095748399dabf3198f53c4d0304
nobukoba <kobayash@rcnp.osaka-u.ac.jp>
2024-06-11 13:19:13 +0900

    modified

--- rawdata_dumper.cc
+++ rawdata_dumper.cc
@@ -98,7 +98,7 @@ int main(int argc, char* argv[]){
 	    std::cout << ", tdc: " << idata.hrtdc;
 	    std::cout << ", tot: " << idata.hrtot << std::endl;
 	  }else if ( stfHeader.femType == 3 || stfHeader.femType == 6 ) { //LRTDC
-	    std::cout << "ch: "<< idata.ch;		
+	    std::cout << ", ch: "<< idata.ch;		
 	    std::cout << ", tdc: "<< idata.tdc;
 	    std::cout << ", tot: "<< idata.tot << std::endl;
 	  }
